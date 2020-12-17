64986e4e6ecc1c14500e6a9432893252e4b3d0d5
zhaopengfei <1027926875@qq.com>
2020-12-17 21:36:10 +0800

    [Spring Security+OAuth2]imooc chapter02 init

---
+++ chap02/uaa/src/main/java/com/imooc/uua/UaaApplication.java
@@ -0,0 +1,15 @@
+package com.imooc.uua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pengfei.zhao
+ * @date 2020/12/17 21:31
+ */
+@SpringBootApplication
+public class UaaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UaaApplication.class, args);
+    }
+}
